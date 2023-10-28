cefb440c299f3ffa1f527a3562b4b0a2d2c6a503
bekzat0047 <abdyraxmanov47@icloud.com>
2023-10-28 15:17:10 +0600

    js

---
+++ index.js
@@ -0,0 +1,41 @@
+// let fruits = ['banan', 'alma', 'kakao', 'juzum', 'koon',]
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits[3]);
+// console.log(fruits[4]);
+
+// fruits.push('arbyz')
+// console.log(fruits);
+
+// for(let i = 0; i < fruits.length; i++){
+//     console.log(fruits[i]);
+// }
+
+// // for (let fruit of fruits){
+// //     console.log(fruit);
+// // }
+
+// let arr = [1, 2, 3, 4, 5, 6, ];
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+// console.log(arr[5]);
+
+// for(let i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+let arr = [10, 28, 37, 45, 56, 62];
+
+for(let number of arr) {
+    // number % 2 === 0 ? console.log(number) : ''
+
+    if(number % 2 === 0){
+        console.log(number);
+    }
+}
+
+
+
